bdf64a6a9a84bd678618bac1ec88311a4c12f73a
Zkeq <learnonly@learnonly.xyz>
2022-02-06 18:57:51 +0800

    修复编辑链接

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/zkeq/Docs/',
+          editUrl: 'https://github.com/zkeq/Docs/tree/main/',
         },
         blog: false,
         theme: {
