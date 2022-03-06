ec5c2ad91172e4dfa93e835f5742d21b68e91904
Zkeq <learnonly@learnonly.xyz>
2022-03-07 01:45:11 +0800

    修正路径

--- src/components/Wechat_Video/index.js
+++ src/components/Wechat_Video/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function WechatVideo({ wxv }) {
-  var v_url = 'http://api.icodeq.com/api/wechat_video_public?wxv_{wxv}'.replace('{wxv}', wxv);
+  var v_url = 'http://api.icodeq.com/api/wechat_video_public?{wxv}'.replace('{wxv}', wxv);
   return (
     <>
       <video width="100%" height="100%" controls="true">
